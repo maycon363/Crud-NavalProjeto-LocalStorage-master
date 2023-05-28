b0c4b8cb0e15ea797dba770b3616a84d9336fb8e
Maycon Borges1~ <mayconborgesgato717@gmail.com>
2023-05-28 15:48:38 -0300

    Adicionado Pagina de informações

---
+++ src/components/Menu.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Container, Dropdown, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+const Menu = () => {
+  return (
+    <div>
+
+      <Navbar bg="success" variant="dark" className="mb-3">
+        <Container>
+          <Navbar.Brand href="/">Frota Naval</Navbar.Brand>
+          <Nav className="me-auto">
+            <Link className="nav-link" to="/">Lista Geral</Link>
+            <Link className="nav-link" to="/solicita">Solicitação de Serviços</Link>
+            <Link className="nav-link" to="/infor">Inforções dos Navios</Link>
+            <Dropdown>
+              <NavDropdown title="Serviços" className='show' id="basic-nav-dropdown">
+                <Link className="dropdown-item" to="/manutencao">Manutenção</Link>
+                <Link className="dropdown-item" to="/construcao">Construção</Link>
+                <Link className="dropdown-item" to="/missao">Missao</Link>
+                <Link className="dropdown-item" to="/treinamento">Treinamento</Link>
+                <Link className="dropdown-item" to="/servico">Serviço</Link>
+                <Link className="dropdown-item" to="/comandantes">Comandantes</Link>
+                <Link className="dropdown-item" to="/armamentos">Armamentos</Link>
+              </NavDropdown>
+            </Dropdown>
+          </Nav>
+        </Container>
+      </Navbar>
+
+    </div>
+  )
+}
+
+export default Menu
